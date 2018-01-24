ead9265fd54be672a6fb02dab6f05f3f6042e47c
AJM-D <31932467+AJM-D@users.noreply.github.com>
2018-01-24 09:55:50 -0400

    Add text ui and button
    Button copies text ui

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.ClipboardManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Text_Field;
+    Button Copy_Button;
+    ClipboardManager cbm;
+    ClipData cd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Copy_Button = (Button) this.findViewById(R.id.Copy_Button);
+        Text_Field = (EditText) this.findViewById(R.id.Text_Field);
+        cbm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
     }
+    public void Copied(View v){
+        cd =ClipData.newPlainText("text", Text_Field.getText().toString());
+        cbm.setPrimaryClip(cd);
+    }
+
 }
