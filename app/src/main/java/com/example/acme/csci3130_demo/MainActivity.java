0d2e563ef84f99d0467eded92478db8bf1480d68
AJM-D <31932467+AJM-D@users.noreply.github.com>
2018-02-10 10:10:13 -0400

    Added methods

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText Text_Field;
+    Button Copy_Button;
+    TextView textAppear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Text_Field = (EditText) this.findViewById(R.id.Text_Field);
+        Copy_Button = (Button) this.findViewById(R.id.Copy_Button);
+        textAppear = (TextView) this.findViewById(R.id.textAppear);
+    }
+    public void Copied(View v){
+        String msg = Text_Field.getText().toString();
+        textAppear.setText(msg);
+    }
+    public boolean notPassword(String m){
+        boolean nP = !m.equalsIgnoreCase("password");
+        return nP;
+    }
+    public boolean atleastEight(String m){
+        boolean nP = m.toString().length()>7;
+        return nP;
     }
 }
